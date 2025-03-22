cc3a23c36ccf794f17067065f5635d29d6b08325
AndersonHJB <bornforthis@bornforthis.cn>
2025-03-23 07:24:33 +0800

    完善脚本命令、以及 README.md

--- run_forest_fire_tests.sh
+++ run_forest_fire_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-nproc=4
+nproc=4  # 设置进程数
 
 for N in $(seq 50 50 200); do
   for p in $(seq 0.1 0.1 0.9); do
-    mpirun -np $nproc ./forest_fire $N $p 50
+    filename="output/output_N${N}_p${p}.txt"
+    echo "Running N=$N, p=$p, M=50" > "$filename"
+    mpirun -np $nproc ./forest_fire $N $p 50 >> "$filename" 2>&1
   done
 done
-
-
